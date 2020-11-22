96137234f045ce123f74ce536c185075cc51d71f
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2020-11-22 20:50:15 +0100

    Slideshow step2

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,12 +1,14 @@
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
+let slides = Array.from(document.querySelectorAll(".slide"));
 let slide2 = document.querySelector(".second-slide");
 let slide3 = document.querySelector(".third-slide");
 let slide1 = document.querySelector(".first-slide");
 // let slideNr = "slide"+"counter";
-
+console.log(slides);
  
+let counter = 0;
 
+next.addEventListener("click", () => {slides[counter + 1].style.display = "block"; slides[counter].style.display = "none"; counter = counter + 1});
 
-next.addEventListener("click", () => {slide2.style.display = "block"; slide1.style.display = "none"});
-
+prev.addEventListener("click", () => {slides[counter - 1].style.display = "block"; slides[counter].style.display = "none"; counter = counter - 1});
