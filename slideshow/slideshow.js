138a41e9eb84205160980e46181b77726bf6a210
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2020-11-24 23:23:37 +0100

    carousel

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -2,15 +2,42 @@ let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
 let slides = Array.from(document.querySelectorAll(".slide"));
 let hides = Array.from(document.querySelectorAll(".hide"));
-// let slide2 = document.querySelector(".second-slide");
-// let slide3 = document.querySelector(".third-slide");
-// let slide1 = document.querySelector(".first-slide");
-// let slideNr = "slide"+"counter";
-console.log(slides);
-console.log(hides); 
-let counter = 0;
-// slides[0].style.display = block;
 
-next.addEventListener("click", () => {slides[counter+1].style.display = "block"; slides[counter].style.display = "none"; counter = counter + 1; console.log(counter)});
-console.log(counter);
-prev.addEventListener("click", () => {slides[counter - 1].style.display = "block"; slides[counter].style.display = "none"; counter = counter - 1});
+let max = slides.length;
+
+console.log(slides);
+console.log(hides);
+let counter = 0;
+
+
+
+next.addEventListener("click", function forward() {
+    if (counter < max - 1) {
+        slides[counter + 1].style.display = "block";
+        slides[counter].style.display = "none"
+        counter = counter + 1;
+    }
+    else if (counter = max) {
+        counter = 0;
+        hides[0].style.display = "block";
+        slides[max - 1].style.display = "none";
+    }
+});
+
+
+prev.addEventListener("click", function backward() {
+    if (counter > 0) {
+
+        slides[counter - 1].style.display = "block";
+        slides[counter].style.display = "none";
+        counter = counter - 1;
+        console.log(counter);
+    }
+    else if (counter === 0) {
+        console.log(counter);
+        counter = max - 1;
+        slides[max - 1].style.display = "block";
+        slides[0].style.display = "none";
+    }
+});
+
