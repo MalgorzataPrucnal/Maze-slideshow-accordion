99f3d3d8c1163f1c42c2cdc40d0d862f15daf58a
Malgorzata Prucnal <=>
2020-12-08 22:47:57 +0100

    Maze - first step

--- maze/maze.js
+++ maze/maze.js
@@ -1,53 +1,53 @@
 const pawn = document.querySelector(".pawn");
 const step = 10;
-
+positionX = 0;
+positionY= 0;
 
 window.addEventListener("keydown", (event)=>
 {
-    const {style} = pawn;
- switch(event.key){
-
-    case "ArrowLeft": 
-    style.left = `${parseInt(style.left)-step}px`;
-    console.log(style.left);
-    break;
-
-    case "ArrowLeft": 
-    style.left = `${parseInt(style.left)+step}px`;
-    console.log(style.left);
-    break;
-
-        case "ArrowUp": 
-        style.top = `${parseInt(style.top)-step}px`;
-        break;
-
-        case "ArrowDown": 
-        style.top = `${parseInt(style.top)+step}px`;
-        break;
- }}
-)
-    // if (event.key === "ArrowLeft"){
-    //     pawn.style.transform = `translateX(${positionX-step}px)`;
-    //     positionX = positionX - step;
+    if (event.key === "ArrowLeft"){
+        pawn.style.transform = `translateX(${positionX-step}px)`;
+        positionX = positionX - step;
      
-    // }
+    }
 
-    // else if (event.key === "ArrowRight"){
-    //     pawn.style.transform = `translateX(${positionX+step}px)`;
-        
-    //     positionX = positionX + step;
-     
-    // }
+    else if (event.key === "ArrowRight"){
+        pawn.style.transform = `translateX(${positionX+step}px)`;
+        console.log("left");
+        positionX = positionX + step;
+    }
 
-    // else if (event.key === "ArrowUp"){
-    //     positionX = positionX;
-    //     pawn.style.transform = `translateY(${positionY-step}px)`;
-    //     positionY = positionY - step;
-      
-    // }
+    else if (event.key === "ArrowUp"){
+        pawn.style.transform = `translateY(${positionY-step}px)`;
+        positionY = positionY - step;
+    }
 
-    // else if (event.key === "ArrowDown"){
-    //     // pawn.style.top = `${positionY+step}px)`;
-    //     pawn.style.transform = `translateY(${positionY+step}px)`;
-    //     positionY = positionY + step;
-    //      }
+    else if (event.key === "ArrowDown"){
+        // pawn.style.top = `${positionY+step}px)`;
+        pawn.style.transform = `translateY(${positionY+step}px)`;
+        positionY = positionY + step;
+         }
+        })
+
+        // const {style} = pawn;
+//  switch(event.key){
+
+//     case "ArrowLeft": 
+//     style.left = `${parseInt(style.left)-step}px`;
+//     console.log(style.left);
+//     break;
+
+//     case "ArrowLeft": 
+//     style.left = `${parseInt(style.left)+step}px`;
+//     console.log(style.left);
+//     break;
+
+//         case "ArrowUp": 
+//         style.top = `${parseInt(style.top)-step}px`;
+//         break;
+
+//         case "ArrowDown": 
+//         style.top = `${parseInt(style.top)+step}px`;
+//         break;
+//  }}
+// )
