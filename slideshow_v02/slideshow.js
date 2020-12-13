939524652a0cd30dd6b88b855d65d694b4889c1d
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2020-12-13 23:34:16 +0100

    slideshow last version

--- slideshow_v02/slideshow.js
+++ slideshow_v02/slideshow.js
@@ -10,33 +10,32 @@ const size = slides[0].clientWidth;
 
 slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
     next.addEventListener("click", () => {
-        // if(counter >= max-1) {console.log("koniec")};
-    slideShow.style.transition = "transform 0.4s ease-in-out";
-    counter ++;
-    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
-    console.log(counter);
+        if(counter > max-2) {return};
+        slideShow.style.transition = "transform 0.6s ease-in-out";
+        counter ++;
+        slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
+        console.log(counter);
 })
 
     prev.addEventListener("click", () => {
-    // if(counter <= 0) return;
-    slideShow.style.transition = "transform 0.4s ease-in-out";
-    counter = counter-1;
-    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
-    console.log(counter);
+        if(counter <= 0) {return};
+        slideShow.style.transition = "transform 0.6s ease-in-out";
+        counter = counter-1;
+        slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
+        console.log(counter);
  })
 
-    slideShow.addEventListener("transitionend", ()=>
-
-    {if (slides[counter].id === "first-picture"){
-    slideShow.style.transition = "none";
-    console.log("none");
-    // counter = slides.length-2;
-    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
+    slideShow.addEventListener("transitionend", ()=>{
+        if (slides[counter].id === "first-picture"){
+            slideShow.style.transition = "none";
+            console.log("none");
+            counter = slides.length-2;
+            slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
 }
     
-    if (slides[counter].id === "last-picture"){
-    slideShow.style.transition = "none";
-    counter = (slides.length)-counter;
-    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
+        if (slides[counter].id === "last-picture"){
+            slideShow.style.transition = "none";
+            counter = (slides.length)-counter;
+            slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
 }
 });
