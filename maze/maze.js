09d36eda41d7103e9a4ef5499a548ea0982b1435
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2020-12-13 20:39:19 +0100

    slideshow2, maze in progress

--- maze/maze.js
+++ maze/maze.js
@@ -1,57 +1,93 @@
-const pawn = document.querySelector(".pawn");
-const step = 10;
-positionX = 0;
-positionY= 0;
+let score = document.querySelector(".score");
 
-window.addEventListener("keydown", (event)=>
-{
-    if (event.key === "ArrowLeft"){
-        pawn.style.transform = `translateX(${positionX-step}px)`;
-        positionX = positionX - step;
-     
+
+const board = [
+    "00010010001",
+    "11001010101",
+    "01101000101",
+    "00000010101",
+    "11101010101",
+    "02001000101",
+    "01111011101",
+    "00001010001",
+    "12101110100",
+    "01100010101",
+    "00001010001",
+  ];
+  
+  if (board[0][4] === "1") {
+    console.log("Wall");
+  }
+  
+  const player = {
+    x: 0,
+    y: 0,
+  };
+  
+  const step = 1;
+  
+  const maze = document.getElementById("maze");
+  
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+  
+  table.append(tbody);
+  
+  maze.append(table);
+  
+  const update = () => {
+    tbody.innerHTML = "";
+    board.forEach((line, y) => {
+      const row = document.createElement("tr");
+  
+      line.split("").forEach((cell, x) => {
+        const td = document.createElement("td");
+  
+        if (cell === "1") {
+          td.classList.add("wall");
+        }
+        if (cell === "2") {
+          td.classList.add("prize");
+        }
+        if (player.x === x && player.y === y) {
+          td.classList.add("player");
+        }
+  
+        row.append(td);
+      });
+  
+      tbody.append(row);
+    });
+  };
+  
+  setInterval(() => update(), 16);
+  
+  window.addEventListener("keydown", (event) => {
+    const pos = { ...player }
+    if (event.key === "ArrowLeft") {
+      pos.x -= step;
+    } else if (event.key === "ArrowRight") {
+      pos.x += step;
+    } else if (event.key === "ArrowUp") {
+      pos.y -= step;
+    } else if (event.key === "ArrowDown") {
+      pos.y += step;
+    }
+    if (board[pos.y][pos.x] === "1") {
+      return;
     }
 
-    else if (event.key === "ArrowRight"){
-        pawn.style.transform = `translateX(${positionX+step}px)`;
-        console.log("left");
-        positionX = positionX + step;
+    if (board[pos.y][pos.x] === "2") {
+
+score.innerText = "100";
+        console.log("100");
+      }
+    if(pos.x === 2 && pos.y === 1){
+        console.log("ok");
     }
-
-    else if (event.key === "ArrowUp"){
-        pawn.style.transform = `translateY(${positionY-step}px)`;
-        positionY = positionY - step;
-    }
-
-    else if (event.key === "ArrowDown"){
-        // pawn.style.top = `${positionY+step}px)`;
-        pawn.style.transform = `translateY(${positionY+step}px)`;
-        positionY = positionY + step;
-
-}
-pawn.style.transform = `translateY(${positionY+step}px) translateX(${positionX + step}px)`;
-})
-
-        
-
-        // const {style} = pawn;
-//  switch(event.key){
-
-//     case "ArrowLeft": 
-//     style.left = `${parseInt(style.left)-step}px`;
-//     console.log(style.left);
-//     break;
-
-//     case "ArrowLeft": 
-//     style.left = `${parseInt(style.left)+step}px`;
-//     console.log(style.left);
-//     break;
-
-//         case "ArrowUp": 
-//         style.top = `${parseInt(style.top)-step}px`;
-//         break;
-
-//         case "ArrowDown": 
-//         style.top = `${parseInt(style.top)+step}px`;
-//         break;
-//  }}
-// )
+    player.x = pos.x;
+    player.y = pos.y;
+    // console.log("x"+pos.x);
+    // console.log("y"+pos.y);
+  });
+  
--- slideshow_v02/slideshow.js
+++ slideshow_v02/slideshow.js
@@ -2,43 +2,41 @@ let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
 let slides = document.querySelectorAll(".slide");
 let slideShow = document.querySelector(".slideshow-container")
-
 let max = slides.length;
-
 let counter = 1;
 const size = slides[0].clientWidth;
 // let last = slides[max+1];
 // let first = slides[0];
 
-slideShow.style.transform = "translateX(" + (-size * counter) + "px)";
-
-next.addEventListener("click", () => {
-    slideShow.style.transition = "transform 0.4s ease-in-out";   
+slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
+    next.addEventListener("click", () => {
+        // if(counter >= max-1) {console.log("koniec")};
+    slideShow.style.transition = "transform 0.4s ease-in-out";
     counter ++;
-    slideShow.style.transform = "translateX(" + (-size * counter) + "px)";
+    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
     console.log(counter);
 })
 
-prev.addEventListener("click", () => {
+    prev.addEventListener("click", () => {
+    // if(counter <= 0) return;
     slideShow.style.transition = "transform 0.4s ease-in-out";
     counter = counter-1;
-    slideShow.style.transform = "translateX(" + (-size * counter) + "px)";
+    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
     console.log(counter);
-    
-})
+ })
 
-slideShow.addEventListener("transitionend", ()=>
-{if (slides[counter].id === "first-picture"){
+    slideShow.addEventListener("transitionend", ()=>
+
+    {if (slides[counter].id === "first-picture"){
     slideShow.style.transition = "none";
     console.log("none");
-    counter = slides.length-2;
-    slideShow.style.transform = "translateX(" + (-size * counter) + "px)";
+    // counter = slides.length-2;
+    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
 }
-
-if (slides[counter].id === "last-picture"){
+    
+    if (slides[counter].id === "last-picture"){
     slideShow.style.transition = "none";
-    console.log("none");
-    counter = slides.length-counter;
-    slideShow.style.transform = "translateX(" + (-size * counter) + "px)";
+    counter = (slides.length)-counter;
+    slideShow.style.transform = "translateX(" + (-100 * counter) + "%)";
 }
 });
