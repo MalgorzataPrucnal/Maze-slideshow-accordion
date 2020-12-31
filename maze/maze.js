569801f008b365ab2d84f7eab79d6a4766b7c8be
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2020-12-31 16:10:55 +0100

    Timer and awards added, WIP

--- maze/maze.js
+++ maze/maze.js
@@ -1,27 +1,29 @@
 let score = document.querySelector(".score");
+let points = 0;
 
-
-const board = [
-    "00010010001",
-    "11001010101",
-    "01101000101",
-    "00000010101",
-    "11101010101",
-    "02001000101",
-    "01111011101",
-    "00001010001",
-    "12101110100",
-    "01100010101",
-    "00001010001",
+var board = [
+    "11111111111111111",
+    "00000010010001000",
+    "11111001010101111",
+    "10001101000101010",
+    "10100000010100010",
+    "10111101010101011",
+    "10012001000101110",
+    "11011111011101000",
+    "11000001010001111",
+    "10012101110100000",
+    "10111103010101111",
+    "10100001110000101",
+    "10101111012010101",
+    "10100001010111101",
+    "10001101012000001",
+    "11111111111111111"
   ];
   
-  if (board[0][4] === "1") {
-    console.log("Wall");
-  }
   
   const player = {
     x: 0,
-    y: 0,
+    y: 1,
   };
   
   const step = 1;
@@ -49,10 +51,13 @@ const board = [
         if (cell === "2") {
           td.classList.add("prize");
         }
-        if (player.x === x && player.y === y) {
-          td.classList.add("player");
+        if (cell === "3") {
+          td.classList.add("extra");
         }
-  
+        if (player.x === x && player.y === y) {
+          td.id = "player";
+        }
+
         row.append(td);
       });
   
@@ -78,16 +83,37 @@ const board = [
     }
 
     if (board[pos.y][pos.x] === "2") {
-
-score.innerText = "100";
-        console.log("100");
-      }
-    if(pos.x === 2 && pos.y === 1){
-        console.log("ok");
+      score.innerText = `${points + 10}`;
+      points = points + 10;
+      let line = board[pos.y];
+      let newLine = line.replace(2, 0);
+      board.splice(pos.y, 1, newLine);
     }
+
     player.x = pos.x;
     player.y = pos.y;
-    // console.log("x"+pos.x);
-    // console.log("y"+pos.y);
+
   });
   
+
+  //STOPPER
+ const placeForTime = document.querySelector(".place-for-time")
+
+  let counter = 20;
+
+
+
+  document.querySelector("button").addEventListener("click", () => {
+    let counting = setInterval(() => {
+      if(counter < 0) {return};
+      counter--;
+      placeForTime.innerHTML = `00 : ${(counter).toString().padStart(2, "0")}`;
+      
+      console.log(counter);
+      if (counter === -1){alert("GAME OVER!!");
+      placeForTime.innerHTML = `00 : 00`}
+    }, 1000);
+    
+        });
+
+    if (counter === 0){clearInterval(counting); };
