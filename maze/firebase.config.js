eee865b27276e0cb3d207b9eb0d378d812b31c6a
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2021-01-15 19:10:13 +0100

    Saving and displaying results added

---
+++ maze/firebase.config.js
@@ -0,0 +1,9 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCWaWwrqIBdeuKjWT8qgr05n_XAx2TqO-o",
+    authDomain: "fir-ex-a5082.firebaseapp.com",
+    projectId: "fir-ex-a5082",
+    storageBucket: "fir-ex-a5082.appspot.com",
+    messagingSenderId: "738122474190",
+    appId: "1:738122474190:web:c744e8f522b8ef0a20669f"
+  };
